7cbc324c983803c4bf77e7d933f0286fa0238848
Zoltan Fodor <me@zoltanfodor.dev>
2024-07-09 15:08:49 +0200

    CHORE: remove unnecessary components/screens

--- src/app/(root)/(tabs)/(index)/feed.tsx
+++ src/app/(root)/(tabs)/(index)/feed.tsx
@@ -1,19 +1,10 @@
-import { View, FlatList } from 'react-native';
-import { Article } from '@srcTypes/Article';
-import { FeedLink } from '@components/Feed/FeedLink';
+import { View } from 'react-native';
+import { StyledText as Text } from '@components/Text/StyledText';
 
-import feedData from '@assets/data/feed.json';
-
-interface RenderItemProps {
-  item: Article;
-}
 export default function Feed() {
   return (
     <View className="flex-1">
-      <FlatList
-        data={feedData}
-        renderItem={({ item }: RenderItemProps) => <FeedLink item={item} />}
-      />
+      <Text>Text</Text>
     </View>
   );
 }
