014059ba6d42574c63ad6a0774d32352380e3d19
Zoltan Fodor <me@zoltanfodor.dev>
2024-07-11 14:57:30 +0200

    CHORE: add a theme inspired by nativewindui and update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,64 @@
 /** @type {import('tailwindcss').Config} */
-import theme from './configuration.json';
-module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  presets: [require("nativewind/preset")],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        light: theme.light,
-        dark: theme.dark,
+import { hairlineWidth, platformSelect } from 'nativewind/theme';
+
+export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
+export const presets = [require("nativewind/preset")];
+export const darkMode = 'class';
+export const theme = {
+  extend: {
+    colors: {
+      border: withOpacity('border'),
+      input: withOpacity('input'),
+      ring: withOpacity('ring'),
+      background: withOpacity('background'),
+      foreground: withOpacity('foreground'),
+      primary: {
+        DEFAULT: withOpacity('primary'),
+        foreground: withOpacity('primary-foreground'),
       },
-      borderRadius: theme.borderRadius,
-      spacing: theme.spacing,
+      secondary: {
+        DEFAULT: withOpacity('secondary'),
+        foreground: withOpacity('secondary-foreground'),
+      },
+      destructive: {
+        DEFAULT: withOpacity('destructive'),
+        foreground: withOpacity('destructive-foreground'),
+      },
+      muted: {
+        DEFAULT: withOpacity('muted'),
+        foreground: withOpacity('muted-foreground'),
+      },
+      accent: {
+        DEFAULT: withOpacity('accent'),
+        foreground: withOpacity('accent-foreground'),
+      },
+      popover: {
+        DEFAULT: withOpacity('popover'),
+        foreground: withOpacity('popover-foreground'),
+      },
+      card: {
+        DEFAULT: withOpacity('card'),
+        foreground: withOpacity('card-foreground'),
+      },
+    },
+    borderWidth: {
+      hairline: hairlineWidth(),
     },
   },
-  plugins: [],
+};
+export const plugins = [];
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return platformSelect({
+        ios: `rgb(var(--${variableName}) / ${opacityValue})`,
+        android: `rgb(var(--android-${variableName}) / ${opacityValue})`,
+      });
+    }
+    return platformSelect({
+      ios: `rgb(var(--${variableName}))`,
+      android: `rgb(var(--android-${variableName}))`,
+    });
+  };
 }
